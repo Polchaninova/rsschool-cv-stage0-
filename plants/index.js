5c891dbe2485734e74b81dcfbe06293111631d77
Olha Polchaninova <polchaninovaolha089@gmail.com>
2023-01-27 19:45:45 +0200

    fix: change js add functional new

--- plants/index.js
+++ plants/index.js
@@ -1,13 +1,27 @@
-// const { height: HeaderBodyHeight } = document
-//   .querySelector(" .header_body")
-//   .getBoundingClientRect();
-// document.body.style.paddingTop = `${HeaderBodyHeight}px`;
+const iconMenu = document.querySelector(`.header_burger`);
+const headerMenu = document.querySelector(`.header_menu`);
+const overlay = document.querySelector(".burger-menu_overlay");
 
-$("DOCTYPE").ready(function () {
-  $(".header_burger").click(function (event) {
-    $(".header_burger,.header_menu").toggleClass("active");
-    $("body").toggleClass("lock");
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle(`lock`);
+    iconMenu.classList.toggle(`active`);
+    headerMenu.classList.toggle(`active`);
   });
-});
+}
 
+const menuLinks = document.querySelectorAll(`.header_link`);
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener(`click`, onMenuLinkClick);
 
+    overlay.addEventListener("click", () => toggleMenuLinks());
+  });
+}
+function onMenuLinkClick(e) {
+  if (iconMenu.classList.contains(`active`)) {
+    document.body.classList.remove(`lock`);
+    iconMenu.classList.remove(`active`);
+    headerMenu.classList.remove(`active`);
+  }
+}
