c1ff6933896f322abc96638ebcc336861b4d1c3f
Olha Polchaninova <polchaninovaolha089@gmail.com>
2023-01-31 19:10:14 +0200

    init: start task -add accordion

--- plants/index.js
+++ plants/index.js
@@ -24,3 +24,44 @@ function onMenuLinkClick(e) {
     headerMenu.classList.remove(`active`);
   }
 }
+// accordion
+const pricesList = document.querySelectorAll(".prices_list");
+
+pricesList.forEach((item, index) => {
+  let headerAccordion = item.querySelector(".header_accordion");
+  headerAccordion.addEventListener("click", () => {
+    item.classList.toggle("open");
+
+    let accordionDescriptionCard = item.querySelector(
+      ".accordion_description_card"
+    );
+    if (item.classList.contains("open")) {
+      accordionDescriptionCard.style.height = `${accordionDescriptionCard.scrollHeight}px`;
+      item
+        .querySelector("img")
+        .classList.replace(".accordion_btn", ".second_btn");
+    } else {
+      accordionDescriptionCard.style.height = "0px";
+      item
+        .querySelector("img")
+        .classList.replace(".second_btn", ".accordion_btn");
+    }
+    removeOpen(index);
+  });
+});
+function removeOpen(index1) {
+  pricesList.forEach((item2, index2) => {
+    if (index1 != index2) {
+      item2.classList.remove("open");
+
+      let accordionDescriptionCard = item2.querySelector(
+        ".accordion_description_card"
+      );
+
+      accordionDescriptionCard.style.height = "0px";
+      item2
+        .querySelector("img")
+        .classList.replace(".second_btn", ".accordion_btn");
+    }
+  });
+}
