eec4881c5cc2628fea2a9c354683ced6ff004416
Olha Polchaninova <polchaninovaolha089@gmail.com>
2023-02-15 13:43:37 +0200

    feat add functional to slade show

--- momentum/src/getRandomNum.js
+++ momentum/src/getRandomNum.js
@@ -1,30 +1,45 @@
-let randomNum;
+import { getTimeOfDay } from "./showGreeting";
+
+let randomNum = getRandomNum(2, "0").toString();
 
 export function getRandomNum(min, max) {
   min = Math.ceil(1);
   max = Math.floor(20);
-  randomNum = getRandomNum;
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export function setBg() {
-  let timeOfDay;
-  console.log(timeOfDay);
-  let bgNum = getRandomNum(1, 20).toString();
-  bgNum.padStart(0, 2);
+function setBg(randomNum) {
+  const img = new Image();
 
-  document.body.style.backgroundImage =
-    "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg')";
+  const randomNumString = randomNum.toString().padStart(2, "0");
+  const url = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${getTimeOfDay()}/${randomNumString}.jpg`;
+
+  img.src = url;
+  img.onload = () => {
+    console.log(url);
+    document.body.style.backgroundImage = `url("${url}")`;
+  };
 }
-setBg();
-export function getSlideNext() {
-  const slideNext = document.querySelector(".slide-next");
+setBg(randomNum);
 
-  slideNext.addEventListener("click", getSlideNext);
+const slidePrev = document.querySelector(".slide-prev");
+slidePrev.addEventListener("click", getSlidePrev);
+
+function getSlidePrev() {
+  randomNum = +randomNum - 1;
+  if (randomNum === 0) {
+    randomNum = 20;
+  }
+  setBg(randomNum);
 }
+const slideNext = document.querySelector(".slide-next");
+slideNext.addEventListener("click", getSlideNext);
 
-export function getSlidePrev() {
-  const slidePrev = document.querySelector(".slide-prev");
-
-  slidePrev.addEventListener("click", getSlidePrev);
+function getSlideNext() {
+  randomNum = +randomNum + 1;
+  if (randomNum === 21) {
+    randomNum = 1;
+  }
+  setBg(randomNum);
 }
